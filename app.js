e131e5f42f7b63aea84af0a0d9790068ed221d5e
Andrew Molostvov <zekneer@gmail.com>
2019-08-04 20:10:30 +0300

    Rework views catalog. Add home router.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const homeRouter = require('./routes/homeRouter');
 
 const app = express();
 app.use('/public', express.static(`${__dirname}/static`));
+
+app.use('/', homeRouter);
 
 app.use((req, res) => {
   res.render('404.pug');
