88cc09cc03debf3dfc127eef10159e9f860fdcea
Andrew Molostvov <zekneer@gmail.com>
2019-08-04 19:31:51 +0300

    Add static files and 404 page middlewares. Base pug layouts.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+app.use('/public', express.static(`${__dirname}/static`));
 
 app.use((req, res) => {
-  res.send('Main page!');
+  res.render('404.pug');
 });
 
 app.listen(3000);
