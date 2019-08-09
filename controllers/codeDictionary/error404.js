8e108b17b925d946cf22f46f9a3719e13735dbc8
Andrew Molostvov <zekneer@gmail.com>
2019-08-09 18:59:08 +0300

    Add error 404 handler to code dictionary route

---
+++ controllers/codeDictionary/error404.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+  res.render('./codeDictionary/404.pug');
+};
--- routes/codeDictionaryRouter.js
+++ routes/codeDictionaryRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const index = require('../controllers/codeDictionary/index');
+const error404 = require('../controllers/codeDictionary/error404');
 
 const codeDictionaryRouter = express.Router();
 
 codeDictionaryRouter.get('/', index);
+codeDictionaryRouter.use(error404);
 
 module.exports = codeDictionaryRouter;
