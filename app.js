e06b1725be15a9d6027f9ba3108688ccac1a2d4d
Andrew Molostvov <zekneer@gmail.com>
2019-08-22 00:42:23 +0300

    Add middleware for parse request body like json

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const codeDictionaryRouter = require('./routes/codeDictionaryRouter');
 const englishTrainerRouter = require('./routes/englishTrainerRouter');
 
 const app = express();
+app.use(express.json());
+
 app.use('/public', express.static(`${__dirname}/static`));
 
 app.use('/codeDictionary', codeDictionaryRouter);
